dbe3a30d1f42b0d87377fd485e4adc010b61f97a
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-24 19:19:43 +0900

    주간 삭제 cloudinary 연동 성공 !!

--- lib/page/delete_weekend_images_page.dart
+++ lib/page/delete_weekend_images_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart';
+import 'package:http/http.dart' as http;
 
 class DeleteImagesPage extends StatefulWidget {
   final String option;
@@ -37,22 +40,92 @@ class _DeleteImagesPageState extends State<DeleteImagesPage> {
   }
 
   Future<void> _deleteImages() async {
-    try {
+    final confirm = await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('사진 삭제'),
+        content: Text('선택한 이미지를 삭제하시겠습니까?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text('취소'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: Text('삭제'),
+          ),
+        ],
+      ),
+    );
+
+    if (confirm == true) {
+      final apiKey = "491384474792879";
+      final apiSecret = "hE8xMCTm7R8q8mf0K_MrlguymiU";
+      final cloudName = "duqykedvy";
+
       for (int i = 0; i < imagesForDeletion.length; i++) {
         if (selectedImages[i]) {
-          await imagesForDeletion[i].reference.delete();
+          final imageId = imagesForDeletion[i].id;
+          final imageData = imagesForDeletion[i].data() as Map<String, dynamic>?;
+          final url = imageData?['url'] as String? ?? '';
+
+          if (url.isNotEmpty) {
+            final parts = url.split('/');
+            final fileName = parts.last.split('.').first;
+            final folderPathParts = parts.sublist(7, parts.length - 1);
+            final decodedPathParts =
+            folderPathParts.map(Uri.decodeComponent).toList();
+            final publicId = decodedPathParts.join('/') + '/' + fileName;
+
+            final timestamp =
+            (DateTime.now().millisecondsSinceEpoch / 1000).round();
+
+            final params = {
+              'public_id': publicId,
+              'timestamp': timestamp.toString(),
+            };
+
+            final paramString = params.entries
+                .map((entry) => '${entry.key}=${entry.value}')
+                .join('&');
+
+            final signature = sha256
+                .convert(utf8.encode('$paramString$apiSecret'))
+                .toString();
+
+            final response = await http.post(
+              Uri.parse(
+                  'https://api.cloudinary.com/v1_1/$cloudName/image/destroy'),
+              body: {
+                ...params,
+                'signature': signature,
+                'api_key': apiKey,
+              },
+              headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            );
+
+            print("Response Status Code: ${response.statusCode}");
+            print("Response Body: ${response.body}");
+
+            if (response.statusCode == 200) {
+              await FirebaseFirestore.instance
+                  .collection('images')
+                  .doc(imageId)
+                  .delete();
+              print("이미지 삭제 완료. ID: $imageId");
+            } else {
+              print(
+                  'Failed to delete image from Cloudinary. Status code: ${response.statusCode}');
+            }
+          } else {
+            print('URL is empty for image ID: $imageId');
+          }
         }
       }
 
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('사진 삭제 완료')),
-      );
-
-      Navigator.pop(context);
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('사진 삭제 중 오류 발생: $e')),
-      );
+      setState(() {
+        _fetchImagesToDelete();
+      });
     }
   }
 
@@ -72,7 +145,8 @@ class _DeleteImagesPageState extends State<DeleteImagesPage> {
         ),
         itemCount: imagesForDeletion.length,
         itemBuilder: (context, index) {
-          final imageData = imagesForDeletion[index].data() as Map<String, dynamic>;
+          final imageData =
+          imagesForDeletion[index].data() as Map<String, dynamic>;
           final imageUrl = imageData['url'] as String;
           return Stack(
             children: [
