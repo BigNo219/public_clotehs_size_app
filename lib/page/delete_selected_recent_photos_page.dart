5e5a24b2d84e772717e532a0f5024f724d0fe000
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-05-07 16:25:56 +0900

    <Flutter> 최근 사진 선택 삭제 - Cloudinary 삭제 적용

--- lib/page/delete_selected_recent_photos_page.dart
+++ lib/page/delete_selected_recent_photos_page.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:provider/provider.dart';
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class DeleteSelectedRecentPhotosPageModel extends ChangeNotifier {
   List<QueryDocumentSnapshot> _imageDocs = [];
@@ -31,11 +36,66 @@ class DeleteSelectedRecentPhotosPageModel extends ChangeNotifier {
   }
 
   Future<void> deleteSelectedImages() async {
+    final apiKey = dotenv.env['API_KEY']!;
+    final apiSecret = dotenv.env['API_SECRET']!;
+    final cloudName = dotenv.env['CLOUD_NAME']!;
+    final cloudinaryUrl = dotenv.env['CLOUDINARY_URL']!;
+    final cloudinaryEndpoint = dotenv.env['CLOUDINARY_URL_ENDPOINT']!;
+
     for (final imageId in _selectedImageIds) {
-      await FirebaseFirestore.instance.collection('images').doc(imageId).delete();
+      final doc = await FirebaseFirestore.instance
+          .collection('images')
+          .doc(imageId)
+          .get();
+      final url = doc.data()?['url'] as String? ?? '';
+
+      if (url.isNotEmpty) {
+        final parts = url.split('/');
+        final fileName = parts.last.split('.').first;
+        final folderPathParts = parts.sublist(7, parts.length - 1);
+        final decodedPathParts =
+        folderPathParts.map(Uri.decodeComponent).toList();
+        final publicId = decodedPathParts.join('/') + '/' + fileName;
+
+        final timestamp =
+        (DateTime.now().millisecondsSinceEpoch / 1000).round();
+
+        final params = {
+          'public_id': publicId,
+          'timestamp': timestamp.toString(),
+        };
+
+        final paramString = params.entries
+            .map((entry) => '${entry.key}=${entry.value}')
+            .join('&');
+
+        final signature =
+        sha256.convert(utf8.encode('$paramString$apiSecret')).toString();
+
+        final response = await http.post(
+          Uri.parse('$cloudinaryUrl$cloudName$cloudinaryEndpoint'),
+          body: {
+            ...params,
+            'signature': signature,
+            'api_key': apiKey,
+          },
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        );
+
+        if (response.statusCode == 200) {
+          await FirebaseFirestore.instance
+              .collection('images')
+              .doc(imageId)
+              .delete();
+        } else {
+          print(
+              'Failed to delete image from Cloudinary. Status code: ${response.statusCode}');
+        }
+      }
     }
+
     _selectedImageIds.clear();
-    fetchImages();
+    await fetchImages();
   }
 }
 
