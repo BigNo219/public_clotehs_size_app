2446988ade4d2f78ea1efa6bddd6866113d9c91c
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-05-10 16:59:31 +0900

    <Flutter> 최근 사진에 ShoppingMall Filter 추가

--- lib/page/recent_photos_page.dart
+++ lib/page/recent_photos_page.dart
@@ -1,5 +1,3 @@
-// recent_photos_page.dart
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ddundddun/page/photo_page.dart';
@@ -8,73 +6,186 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 class RecentPhotosPageModel extends ChangeNotifier {
   List<QueryDocumentSnapshot> _imageDocs = [];
+  List<String> _selectedFilters = [];
+  DocumentSnapshot? _lastDocument;
+  bool _isLoading = false;
 
   List<QueryDocumentSnapshot> get imageDocs => _imageDocs;
+  List<String> get selectedFilters => _selectedFilters;
+  bool get isLoading => _isLoading;
 
   Future<void> fetchImages() async {
-    final snapshot = await FirebaseFirestore.instance
+    if (_isLoading) return;
+
+    _isLoading = true;
+    notifyListeners();
+
+    Query query = FirebaseFirestore.instance
         .collection('images')
         .orderBy('timestamp', descending: true)
-        .limit(20) // 사진 가져오는 갯수
-        .get();
+        .limit(24);
 
-    _imageDocs = snapshot.docs;
+    if (_lastDocument != null) {
+      query = query.startAfterDocument(_lastDocument!);
+    }
+
+    if (_selectedFilters.isNotEmpty) {
+      query = query.where('shoppingMalls', arrayContainsAny: _selectedFilters);
+    }
+
+    final snapshot = await query.get();
+
+    _imageDocs.addAll(snapshot.docs);
+    _lastDocument = snapshot.docs.isNotEmpty ? snapshot.docs.last : null;
+
+    _isLoading = false;
     notifyListeners();
+  }
+
+  void toggleFilter(String filter) {
+    if (_selectedFilters.contains(filter)) {
+      _selectedFilters.remove(filter);
+    } else {
+      _selectedFilters.add(filter);
+    }
+    resetImages();
+  }
+
+  void resetImages() {
+    _imageDocs.clear();
+    _lastDocument = null;
+    fetchImages();
   }
 }
 
-class RecentPhotosPage extends StatelessWidget {
+class RecentPhotosPage extends StatefulWidget {
+  @override
+  _RecentPhotoPageState createState() => _RecentPhotoPageState();
+}
+
+class _RecentPhotoPageState extends State<RecentPhotosPage> {
+  @override
+  void dispose() {
+    super.dispose();
+    PaintingBinding.instance.imageCache.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => RecentPhotosPageModel()..fetchImages(),
-      child: Scaffold(
-        body: Consumer<RecentPhotosPageModel>(
-          builder: (context, model, child) {
-            final imageDocs = model.imageDocs;
-            if (imageDocs.isEmpty) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            return GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-                childAspectRatio: 1,
-              ),
-              padding: EdgeInsets.all(8),
-              itemCount: imageDocs.length,
-              itemBuilder: (context, index) {
-                final imageDoc = imageDocs[index];
-                final imageUrl = imageDoc['url'];
-                final imageId = imageDoc.id;
-                final subCategory = imageDoc['subCategory'];
-                return GestureDetector(
-                  onTap: () => _openPhotoPage(context, imageUrl, subCategory, imageId),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: CachedNetworkImage(
-                      imageUrl: imageUrl,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
-                      errorWidget: (context, url, error) => const Icon(Icons.error),
+      child: Column(
+        children: [
+          _buildFilterCheckboxes(),
+          Expanded(
+            child: Consumer<RecentPhotosPageModel>(
+              builder: (context, model, child) {
+                final imageDocs = model.imageDocs;
+                if (imageDocs.isEmpty) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return NotificationListener<ScrollNotification>(
+                  onNotification: (ScrollNotification scrollInfo) {
+                    if (!model.isLoading &&
+                        scrollInfo.metrics.pixels ==
+                            scrollInfo.metrics.maxScrollExtent) {
+                      model.fetchImages();
+                    }
+                    return true;
+                  },
+                  child: GridView.builder(
+                    gridDelegate:
+                    const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                      childAspectRatio: 1,
                     ),
+                    padding: EdgeInsets.all(8),
+                    itemCount: imageDocs.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index < imageDocs.length) {
+                        final imageDoc = imageDocs[index];
+                        final imageUrl = imageDoc['url'];
+                        final imageId = imageDoc.id;
+                        final subCategory = imageDoc['subCategory'];
+                        return GestureDetector(
+                          onTap: () => _openPhotoPage(
+                              context, imageUrl, subCategory, imageId),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: CachedNetworkImage(
+                              imageUrl: imageUrl,
+                              fit: BoxFit.cover,
+                              placeholder: (context, url) => const Center(
+                                  child: CircularProgressIndicator()),
+                              errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                            ),
+                          ),
+                        );
+                      } else if (model.isLoading) {
+                        return const Center(
+                            child: CircularProgressIndicator());
+                      } else {
+                        return const SizedBox.shrink();
+                      }
+                    },
                   ),
                 );
               },
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  void _openPhotoPage(BuildContext context, String? imageUrl, String? subCategory, String? imageId) {
+  Widget _buildFilterCheckboxes() {
+    return Consumer<RecentPhotosPageModel>(
+      builder: (context, model, child) {
+        return Container(
+          height: 40,
+          color: Colors.grey[200],
+          padding: EdgeInsets.all(3.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildFilterCheckbox('UNBUTTY', model),
+              _buildFilterCheckbox('MYUARIN', model),
+              _buildFilterCheckbox('NONBETTER', model),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildFilterCheckbox(String label, RecentPhotosPageModel model) {
+    return Row(
+      children: [
+        Checkbox(
+          value: model.selectedFilters.contains(label),
+          onChanged: (value) => model.toggleFilter(label),
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 10,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _openPhotoPage(BuildContext context, String? imageUrl,
+      String? subCategory, String? imageId) {
     if (imageUrl != null && subCategory != null && imageId != null) {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => PhotoPage(imageUrl: imageUrl, category: subCategory, imageId: imageId),
+          builder: (context) => PhotoPage(
+              imageUrl: imageUrl, category: subCategory, imageId: imageId),
         ),
       );
     } else {
