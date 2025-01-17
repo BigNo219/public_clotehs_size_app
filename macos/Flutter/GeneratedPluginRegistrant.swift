60caef856bfa0985a0983422db209e3bebc2541e
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2025-01-17 12:45:21 +0900

    feat: pubspec.lock 내 의존성 패키지 버전 업데이트
    - 여러 패키지의 최신 버전으로 업데이트: firebase_auth, cloud_firestore 등
    - 업데이트된 패키지: flutterfire_internals, dio, camera 등
    - 성능 향상 및 신규 기능 지원을 위해 의존성 최신화

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import firebase_auth
 import firebase_core
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
